8db842364dd634d93cbbe999a8134eab875d0d43
Ashley Stock <stock@astro.utoronto.ca>
2020-01-28 14:42:27 -0500

    Made some optimizations and plotting changing velocity

--- RickettEquations.py
+++ RickettEquations.py
@@ -21,25 +21,25 @@ from astropy.table import Table
 i = 88.7*u.deg
 s = 0.71
 Oangle = 61*u.deg
-R = 0.19
-PsiAR = 47*u.deg
-VIS = np.array([-21,29])*u.km/u.s
+R = 0.71
+PsiAR = 61*u.deg
+VIS = np.array([-9,42])*u.km/u.s
 s0 = 4.2e6*u.m
 #--------------------------------------------------------------------------------------
 # Other constant values
 #--------------------------------------------------------------------------------------
 par = "J0737-3039A.par" #parameter file from ATNF
-t_start = 53467 #52997
-t_end = 53468 #53560
-t_nsteps = 2 #563
-plot_TISS = True #toggle whether the interstellar scintillation timescale is plotted
+t_start = 52997 #52997
+t_end = 53561 #53561
+t_nsteps = 564 #564
+plot_TISS = False #toggle whether the interstellar scintillation timescale is plotted
 plot_knorm_all = False #toggle whether the normalized harmonic coefficients are plotted
-plot_knorm_VIS = False
+plot_knorm_VIS = False #toggle normalized harmonic coefficients being plotted for different velocities
 lt_s = u.Unit('lt_s', u.lightyear / u.yr * u.s)
 
 #---------------------------------------------------------------------------------------
 
-def Q_coeff(R,Psi_AR):
+def Q_coeff(R,Psi_Psi_AR):
 	"""This function calculates the quadratic coefficients which
 	describe the ISS anisotropy as defined in equation 4 of Rickett et al. 2014
 	Parameters:
@@ -58,9 +58,9 @@ def Q_coeff(R,Psi_AR):
 def OrbitMeanVel(PB,SMA,ECC):
 	"""This function calculates the mean orbital velocity of the pulsar.
 	Parameters:
-		PB: the orbital period of the pulsar, float
-		SMA: the semi major axis, float
-		ECC: the orbital eccentricity
+		PB: the orbital period of the pulsar (units time), float
+		SMA: the semi major axis (units length), float
+		ECC: the orbital eccentricity (unitless)
 	Returns:
 		V0: the mean orbital velocity in km/s, float
 	"""
@@ -69,8 +69,8 @@ def OrbitMeanVel(PB,SMA,ECC):
 def SpatialScale(s0,s):
 	"""This function calculates the unitless spatial scale in the pulsar frame.
 	Parameters:
-		s0: the mean diffractive scale (unitless), float
-		s: the fractional distance from the pulsar to the scintillation screen, float
+		s0: the mean diffractive scale (units of length), float
+		s: the fractional distance from the pulsar to the scintillation screen (unitless), float
 	Returns:
 		sp: the spatial scale in the pulsar frame, float
 	"""
@@ -109,6 +109,7 @@ def PulsarBCVelocity(psr):
 
 def RotateVector(v,angle):
 	"""This function rotates a vector from RA-DEC coordinates to pulsar orbital frame coordinates.
+	If you are using it generally, note that this 
 	Parameters:
 		v: a np array with proper motions in km/s in order (PM_RA,PM_DEC), float
 		angle: the angle needed to rotate onto x-y plane in radians, float
@@ -119,7 +120,7 @@ def RotateVector(v,angle):
 	return [new_v[0].value, new_v[1].value]*u.km/u.s
 
 
-def SystemVel(t_start,t_end,s,Oangle,psr,VIS):
+def SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS):
 	"""This function calculates the system velocity in the pulsar frame
 	as defined in equation 6 of Rickett et al. 2014.
 	Parameters:
@@ -134,10 +135,11 @@ def SystemVel(t_start,t_end,s,Oangle,psr,VIS):
 	times = Time(np.array(range(t_start,t_end)), format = 'mjd')
 	
 	#Calculate Earth velocity
-	VE = []
+	VE = np.ones((t_nsteps,2))
+	i=0
 	for t in times:
-		VE.append(RotateVector(EarthVelocity(t,'gbt',psr,0*u.deg)[1:3],Oangle))
-	VE = np.array(VE)
+		VE[i]=RotateVector(EarthVelocity(t,'gbt',psr,0*u.deg)[1:3],Oangle)
+		i +=1
 
 	#Calculate pulsar velocity
 	VP = np.array([-17.8,11.6])*u.km/u.s #Just use values from paper for now
@@ -146,9 +148,11 @@ def SystemVel(t_start,t_end,s,Oangle,psr,VIS):
 	#Otherwise
 	#VP = PulsarBCVelocity(psr)[1:3] 
 
-	VC = []
+	VC = np.ones((t_nsteps,2))
+	i = 0
 	for v in VE:
-		VC.append(np.add(np.add(VP,v*u.km/u.s*s/(1-s)),-VIS/(1-s)))
+		VC[i] = np.add(np.add(VP,v*u.km/u.s*s/(1-s)),-VIS/(1-s))
+		i +=1
 	return np.array(VC)*u.km/u.s
 
 #TODO: Maybe make this so it works for VC at multiple times
@@ -236,7 +240,6 @@ if (plot_TISS or plot_knorm_all or plot_knorm_VIS):
 
 	psr_m.delay(t) #NOTE: You can only get binary model values IFF you call psr_m.delay(t) first!!!
 	bm = psr_m.binary_instance
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
 
 if (plot_TISS):
 
@@ -254,15 +257,16 @@ if (plot_TISS):
 	#phi = (bm.nu() + bm.omega())%(2*math.pi)
 
 	s0 = 6.6e6*u.m
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 
 	#Calculate K coefficients
 	Ks0 = K_coeffs(OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),VC[0],Q_coeff(R,PsiAR),i,psr_m.OM.quantity,psr_m.ECC.quantity,s0/(1-s))
 	print('K Coefficients for i = 90', Ks0)
 
-	TS_plot = []
+	TS_plot = np.ones(phi.size)
 
 	for angle in phi:
-		TS_plot.append(TISS(Ks0,angle))
+		TS_plot[np.where(phi==angle)] = TISS(Ks0,angle)
 
 	ax.plot(np.degrees(phi),TS_plot, label='i=90.0$^\circ$')
 
@@ -275,16 +279,17 @@ if (plot_TISS):
 	VIS = np.array([-79,100])*u.km/u.s
 	s0 = 10.1e6*u.m
 
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 
 	Ks0 = K_coeffs(OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),VC[0],Q_coeff(R,PsiAR),i,psr_m.OM.quantity,psr_m.ECC.quantity,s0/(1-s))
 
 	print('K Coefficients for i = 91.3', Ks0)
 
-	TS_plot = []
+	TS_plot = np.ones(phi.size)
 
 	for angle in phi:
-		TS_plot.append(TISS(Ks0,angle))
+		TS_plot[np.where(phi==angle)] = TISS(Ks0,angle)
+
 
 	ax.plot(np.degrees(phi),TS_plot, label='i=91.3$^\circ$')
 
@@ -298,16 +303,17 @@ if (plot_TISS):
 	VIS = np.array([-9,42])*u.km/u.s
 	s0 = 6.1e6*u.m
 
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 
 	Ks0 = K_coeffs(OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),VC[0],Q_coeff(R,PsiAR),i,psr_m.OM.quantity,psr_m.ECC.quantity,s0/(1-s))
 
 	print('K Coefficients for i = 88.7', Ks0)
 
-	TS_plot = []
+	TS_plot = np.ones(phi.size)
 
 	for angle in phi:
-		TS_plot.append(TISS(Ks0,angle))
+		TS_plot[np.where(phi==angle)] = TISS(Ks0,angle)
+
 
 	ax.plot(np.degrees(phi),TS_plot, label='i=88.7$^\circ$')
 
@@ -320,6 +326,7 @@ if (plot_TISS):
 	plt.show()
 
 if (plot_knorm_all):
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 	uw = uw_fit(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
 	times = np.array(range(t_start-53000,t_end-53000))
@@ -351,7 +358,7 @@ if (plot_knorm_all):
 	PsiAR = 61*u.deg
 	VIS = np.array([-9,42])*u.km/u.s
 
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 
 	uw = uw_fit(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
@@ -388,9 +395,10 @@ if (plot_knorm_VIS):
 	Oangle = 111*u.deg
 	R = 0.96
 	PsiAR = 118*u.deg
-	VIS = np.array([-79,100])*u.km/u.s
+	VIS0 = np.array([-79,100])*u.km/u.s
 
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS0)
+
 
 	uw = uw_fit(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
@@ -399,24 +407,28 @@ if (plot_knorm_VIS):
 	plt.plot(times,4*uw[0]+2*uw[2]*uw[1],"r-", label='VIS,y = 100 km/s')
 	plt.plot(times,-1-2*uw[0]*uw[0]-2*uw[2]*uw[0]*uw[1]-2*uw[1]*uw[1],"r--")
 
-
-	#Values from Column 2
-	VIS = np.array([-79,200])*u.km/u.s
+	VIS = RotateVector(VIS0,-PsiAR) #Rotate into parallel and perpendicular axes
+	VIS[1] -= 50*u.km/u.s #Add to perpendicular axis
+	VIS = -RotateVector(VIS,PsiAR) #Rotate back into x and y
+	print('VIS changed perp: ', VIS)
 	
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 	uw = uw_fit(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
-	plt.plot(times,4*uw[0]+2*uw[2]*uw[1],"b-", label='VIS,y = 200 km/s')
+	plt.plot(times,4*uw[0]+2*uw[2]*uw[1],"b-", label='VIS,perp += 50 km/s')
 	plt.plot(times,-1-2*uw[0]*uw[0]-2*uw[2]*uw[0]*uw[1]-2*uw[1]*uw[1],"b--")
 
-	#Values from Column 3
-	VIS = np.array([-79,300])*u.km/u.s
+	VIS = RotateVector(VIS0,-PsiAR) #Rotate into parallel and perpendicular axes
+	VIS[0] += 50*u.km/u.s #Add to parallel axis
+	VIS = -RotateVector(VIS,PsiAR) #Rotate back into x and y
 
-	VC = SystemVel(t_start,t_end,s,Oangle,psr,VIS)
+	print('VIS changed parallel: ', VIS)
+
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 
 	uw = uw_fit(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
-	plt.plot(times,4*uw[0]+2*uw[2]*uw[1],"g-", label='VIS,y = 300 km/s')
+	plt.plot(times,4*uw[0]+2*uw[2]*uw[1],"g-", label='VIS,par += 50 km/s')
 	plt.plot(times,-1-2*uw[0]*uw[0]-2*uw[2]*uw[0]*uw[1]-2*uw[1]*uw[1],"g--")
 	
 
