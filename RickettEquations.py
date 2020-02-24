01b7efba86a396fa0ed8a5179db169d57109613d
Ashley Stock <stock@astro.utoronto.ca>
2020-02-24 10:48:01 -0500

    Arc Curvature calculated with PINT phases

--- RickettEquations.py
+++ RickettEquations.py
@@ -36,6 +36,7 @@ t_nsteps = 564 #564
 plot_TISS = False #toggle whether the interstellar scintillation timescale is plotted
 plot_knorm_all = False #toggle whether the normalized harmonic coefficients are plotted
 plot_knorm_VIS = False #toggle normalized harmonic coefficients being plotted for different velocities
+plot_knorm_VIS1 = True
 lt_s = u.Unit('lt_s', u.lightyear / u.yr * u.s)
 
 #---------------------------------------------------------------------------------------
@@ -158,7 +159,6 @@ def SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS):
 		i +=1
 	return np.array(VC)*u.km/u.s
 
-#TODO: Maybe make this so it works for VC at multiple times
 def K_coeffs(V0,VC,Qabc,i,omega,ecc,sp):
 	"""This function calculates the orbital harmonic coefficients for the scintillation
 	timescale as defined in equation 10 of Rickett et al. 2014.
@@ -227,12 +227,12 @@ def k_norm(VC,m,bm,V0,Qabc,i):
 	w = Qabc[2]/math.sqrt(Qabc[0]*Qabc[1])
 	return [4*ux+2*w*uy,-1-2*ux*ux - 2*w*ux*uy - 2*uy*uy]
 
-def eta(t_start,t_end,i,s,Oangle,PsiAR,VIS,par,dpsr,freq,phase):
+def eta(t_start,t_end,t_nsteps,i,s,Oangle,PsiAR,VIS,par,dpsr,freq):
 	"""
 	This function calculates the expected curvature of scintillation arcs.
 	Parameters:
-		t_start:  The epoch that the curvature is being calculated for,
-			and integer in MJD
+		t_start:  The start of the epoch that the curvature is being calculated for in MJD
+		t_end: The end of the epoch that the curvature is being calculated for in MJD
 		i : the inclination angle of the orbit, float
 		s : the fractional distance from the pulsar to the scintillation screen, float
 		Oangle : the angle needed to rotate onto x-y plane in radians, float
@@ -242,39 +242,36 @@ def eta(t_start,t_end,i,s,Oangle,PsiAR,VIS,par,dpsr,freq,phase):
 		par : parameter filename, string
 		dpsr : distance to the pulsar, float
 		freq : frequency of interest, float
-		phase : the orbital phase of the pulsar from the line of nodes (i.e. true anomaly + longitude 				of periastron), a numpy array of floats
 	Returns:
+		VA : The scintillation velocity parallel to the screen
 		eta : the expected curvature of the scintillation arcs as a function of orbital phase 
 	"""
 	psr_m = get_model(par) # create model of pulsar
 
 	psr = SkyCoord(ra=str(psr_m.RAJ.quantity), dec=str(psr_m.DECJ.quantity), pm_ra_cosdec=psr_m.PMRA.quantity, pm_dec=psr_m.PMDEC.quantity, distance=dpsr)
 
-	#Calculate values from binary model for one orbit
-	#t = toa.make_fake_toas(t_start,t_start+psr_m.PB.value,phase.shape[0],psr_m,freq=freq,obs="GBT")
-	t = toa.make_fake_toas(t_start,t_end,phase.shape[0],psr_m,freq=freq,obs="GBT")
+	#Calculate values from binary model for given time frame
+	t = toa.make_fake_toas(t_start,t_end,t_nsteps,psr_m,freq=freq,obs="GBT")
 	psr_m.delay(t)
 	bm = psr_m.binary_instance
-	#phase = bm.nu()+bm.omega()
-	#print('True Anomaly: ', bm.nu())
-	#print('Longitude of Periastron: ', bm.omega())
+	phase = bm.nu()+bm.omega()
 
 	#Calculate scintillation velocity
-	VC = SystemVel(t_start,t_start+1,1,s,Oangle,psr,VIS)
+	VC = SystemVel(int(t_start),int(t_start)+1,1,s,Oangle,psr,VIS)
 	V0 = OrbitMeanVel(psr_m.PB.quantity,psr_m.A1.quantity/psr_m.SINI.quantity,psr_m.ECC.quantity)
 	VAx = VC[0][0] - V0*psr_m.ECC.quantity*np.sin(bm.omega()) - V0*np.sin(phase)
 	VAy = VC[0][1] + np.cos(i)*(V0*psr_m.ECC.quantity*np.cos(bm.omega()) + V0*np.cos(phase))
 	VA = RotateVector([VAx,VAy],-PsiAR)
 
 	eta = const.c*dpsr*s/(2*freq*freq*(1-s)*VA[0]*VA[0]) 
-	return VA[0], eta	
+	return -VA[0], eta	
 
 
 #----------------------------------------
 # Set up binary pulsar parameters	#
 #----------------------------------------
 
-if (plot_TISS or plot_knorm_all or plot_knorm_VIS):
+if (plot_TISS or plot_knorm_all or plot_knorm_VIS or plot_knorm_VIS1):
 	psr_m = get_model(par)#Read in known values for par file (from ATNF)
 
 	SMA = (psr_m.A1.quantity/psr_m.SINI.quantity) #convert projected semi major axis to actual value
@@ -454,18 +451,20 @@ if (plot_knorm_VIS):
 	plt.plot(times,k0,"r--")
 
 	VIS = RotateVector(VIS0,-PsiAR) #Rotate into parallel and perpendicular axes
-	VIS[1] -= 50*u.km/u.s #Add to perpendicular axis
+	print('VIS after rotation: ', VIS)
+	VIS[1] -= 50*u.km/u.s #Add to parallel axis
+	print('VIS increased parallel: ', VIS)
 	VIS = -RotateVector(VIS,PsiAR) #Rotate back into x and y
-	#print('VIS changed perp: ', VIS)
 	
 	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
 	[ks,k0] = k_norm(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
-	plt.plot(times,ks,"b-", label='VIS,perp += 50 km/s')
+	plt.plot(times,ks,"b-", label='VIS,par += 50 km/s')
 	plt.plot(times,k0,"b--")
 
 	VIS = RotateVector(VIS0,-PsiAR) #Rotate into parallel and perpendicular axes
-	VIS[0] += 50*u.km/u.s #Add to parallel axis
+	VIS[0] += 50*u.km/u.s #Add to perpendicular axis
+	print('VIS increased perpendicular: ', VIS)
 	VIS = -RotateVector(VIS,PsiAR) #Rotate back into x and y
 
 	#print('VIS changed parallel: ', VIS)
@@ -474,7 +473,7 @@ if (plot_knorm_VIS):
 
 	[ks,k0] = k_norm(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
 
-	plt.plot(times,ks,"g-", label='VIS,par += 50 km/s')
+	plt.plot(times,ks,"g-", label='VIS,perp += 50 km/s')
 	plt.plot(times,k0,"g--")
 	
 
@@ -499,5 +498,70 @@ if (plot_knorm_VIS):
 	plt.errorbar(obs_day,ks,yerr=ks_error, fmt='ks',label='ks')
 	plt.legend()
 	plt.show()
+if (plot_knorm_VIS1):
+	#Values from Column 2
+	i = 90*u.deg
+	s = 0.71
+	Oangle = 69*u.deg
+	R = 0.76
+	PsiAR = 72*u.deg
+	VIS0 = np.array([-12,50])*u.km/u.s
 
 
+	times = np.array(range(t_start-53000,t_end-53000))
+
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS0)
+
+
+	[ks,k0] = k_norm(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
+
+
+	plt.plot(times,ks,"r-", label='Best Fit')
+	plt.plot(times,k0,"r--")
+
+	VIS = RotateVector(VIS0,-PsiAR) #Rotate into parallel and perpendicular axes
+	print('VIS after rotation: ', VIS)
+	VIS[1] = VIS[1]/2 #Add to parallel axis
+	print('VIS decreased parallel: ', VIS)
+	VIS = -RotateVector(VIS,PsiAR) #Rotate back into x and y
+	
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
+	[ks,k0] = k_norm(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
+
+	plt.plot(times,ks,"b-", label='VIS,par = VIS,par/2')
+	plt.plot(times,k0,"b--")
+
+	VIS = RotateVector(VIS0,-PsiAR) #Rotate into parallel and perpendicular axes
+	VIS[1] = 0*u.km/u.s #Add to parallel axis
+	print('VIS decreased parallel: ', VIS)
+	VIS = -RotateVector(VIS,PsiAR) #Rotate back into x and y
+
+	VC = SystemVel(t_start,t_end,t_nsteps,s,Oangle,psr,VIS)
+
+	[ks,k0] = k_norm(VC,psr_m,bm,OrbitMeanVel(psr_m.PB.quantity,SMA,psr_m.ECC.quantity),Q_coeff(R,PsiAR),i)
+
+	plt.plot(times,ks,"g-", label='VIS,par = 0 km/s')
+	plt.plot(times,k0,"g--")
+	
+
+	#observation days from Table 1
+	obs_day = [-3,211,311,379,467,202,203,274,312,319,374,378,415,451,462,505,560]
+
+	#k_fit[0] is K0 (col. 1 of Table 1), k_fit[1] is Ks (col.2 of Table 1), k_fit[2] is Kc2 (col. 5 of Table 1)
+	k_fit = np.array([[3.46,3.79,4.76,6.19,3.25,0.96,0.89,0.92,1.35,1.41,0.91,0.94,0.74,0.84,0.74,0.54,0.29],[-2.63,0.70,-3.63,-6.74,-1.57,0.41,0.29,-0.40,-1.23,-1.39,-1.02,-1.05,-0.80,-0.71,-0.60,0.06,0.16],[-2.77,-3.51,-4.02,-4.40,-2.57,-1.07,-0.86,-0.84,-1.15,-1.13,-0.59,-0.62,-0.53,-0.67,-0.58,-0.49,-0.26]])
+
+	#k_err[0] is K0 (col. 1 of Table 1), k_err[1] is Ks (col.2 of Table 1), k_err[2] is Kc2 (col. 5 of Table 1)
+	k_err = np.array([[0.06,0.10,0.13,0.14,0.08,0.09,0.05,0.06,0.06,0.05,0.05,0.03,0.04,0.03,0.02,0.03,0.01],[0.08,0.11,0.16,0.19,0.10,0.15,0.07,0.08,0.07,0.07,0.09,0.05,0.06,0.05,0.03,0.04,0.02],[0.06,0.10,0.12,0.12,0.08,0.12,0.05,0.06,0.06,0.05,0.06,0.03,0.04,0.04,0.02,0.04,0.01]])
+
+	k0 = k_fit[0]/k_fit[2]
+	k0_error = k0*(k_err[0]/k_fit[0] + k_err[2]/k_fit[2])
+
+	ks = k_fit[1]/k_fit[2]
+	ks_error = ks*(k_err[1]/k_fit[1] + k_err[2]/k_fit[2])
+
+	plt.xlabel('MJD-53000')
+	plt.ylabel('Coefficient Value')
+	plt.errorbar(obs_day,k0,yerr=k0_error, fmt='kx', label='k0')
+	plt.errorbar(obs_day,ks,yerr=ks_error, fmt='ks',label='ks')
+	plt.legend()
+	plt.show()
